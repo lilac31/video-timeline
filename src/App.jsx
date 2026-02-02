afece87918591458a8ca23d56e3027a31b78f648
xiaoyeye <xiaoyeye@xiaoyeyedeMacBook-Pro.local>
2026-02-02 15:57:19 +0800

    Use HashRouter for better GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import MediaSelector from './pages/MediaSelector'
 import Timeline from './pages/Timeline'
 
 function App() {
   return (
-    <Router basename="/video-timeline">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/select" element={<MediaSelector />} />
